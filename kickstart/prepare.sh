59ddd258342c48a6bbd4f9bae885ef3c5f8cd494
Yann Collette <ycollette.nospam@free.fr>
2016-03-05 15:33:28 +0100

    add a script to prepare home repo for usb bootable distrib

---
+++ kickstart/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/prepare/lescuizines/SoundFonts
+mkdir -p ~/prepare/lescuizines/GuitarPro
+cp /home/collette/SoundFonts/63mg\ The\ Xioad\ Bank.sf2 ~/prepare/lescuizines/SoundFonts
+cp /home/collette/SoundFonts/SF2/Bass/336-Squierbass.sf2 ~/prepare/lescuizines/SoundFonts
+cp /home/collette/SoundFonts/SF2/Guitar/Guitar\ Distortion.SF2 ~/prepare/lescuizines/SoundFonts
+
+cp -r /home/collette/TuxGuitar/GuitarPro/Cake ~/prepare/lescuizines/GuitarPro/Cake
+cp -r /home/collette/TuxGuitar/GuitarPro/ChuckBerry ~/prepare/lescuizines/GuitarPro/ChuckBerry
